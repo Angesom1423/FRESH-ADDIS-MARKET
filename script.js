ba1c3d23686b8b8f5fca2aa937b09848f0daa4ce
Angesom1423 <angea.gere1423@gmail.com>
2026-01-02 15:52:42 +0100

    rewrite

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//navigation icon function
 const menuIcon = document.getElementById("menuIcon");
 const menu = document.getElementById("menu");
 
@@ -5,3 +6,14 @@ menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("active");
   menu.classList.toggle("show");
 });
+//login format functioning code block
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  if (username === "admin" && password === "1234") {
+    //when click the login icon the page goes to the product page
+    window.location.href = "product.html";
+  } else {
+    alert("Wrong username or password");
+  }
+}
