5abbd1a2fc174cb5450075571adfd21803c4cab1
Angesom1423 <angea.gere1423@gmail.com>
2026-01-04 13:31:58 +0100

    finished

--- script.js
+++ script.js
@@ -1,40 +1,11 @@
-//navigation icon function
+// MENU
 const menuIcon = document.getElementById("menuIcon");
 const menu = document.getElementById("menu");
 
-menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("active");
-  menu.classList.toggle("show");
-});
-async function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email: email,
-    password: password
+if (menuIcon) {
+  menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("active");
+    menu.classList.toggle("show");
   });
-
-  if (error) {
-    alert(error.message);
-  } else {
-    window.location.href = "home.html";
-  }
 }
 
-async function signup() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  const { data, error } = await supabase.auth.signUp({
-    email: email,
-    password: password
-  });
-
-  if (error) {
-    alert(error.message);
-  } else {
-    alert("Signup successful! You can now login.");
-    window.location.href = "index.html";
-  }
-}
